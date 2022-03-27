c9a4683b975a5faae22d72676d93f81b8bbb0ee0
Faithfulness Alamu <vaguemail369@gmail.com>
2022-03-27 20:02:33 +0100

    Use bundled configs in main

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/rs/zerolog"
+	"github.com/thealamu/linkedinsignin/config"
 	"github.com/thealamu/linkedinsignin/linkedin"
 	"github.com/thealamu/linkedinsignin/server"
 	"os"
@@ -13,10 +14,12 @@ var defaultWriter = zerolog.ConsoleWriter{Out: os.Stdout}
 func main() {
 	appLogger := zerolog.New(defaultWriter).With().Timestamp().Logger()
 
-	apiKey, err := getAPIKey()
+	env, err := config.New()
 	if err != nil {
-		appLogger.Fatal().Err(err).Msg("Failed to get API key")
+		appLogger.Fatal().Err(err).Msg("Failed to load configs")
 	}
+
+	apiKey := env[config.ProxyCurlApiKey]
 	linkedinService := linkedin.New(appLogger, apiKey)
 
 	if err := server.Start(appLogger, linkedinService); err != nil {
