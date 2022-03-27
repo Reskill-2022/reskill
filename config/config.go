b256b2a279fba6a064c79f68609f609871ef8eae
Faithfulness Alamu <vaguemail369@gmail.com>
2022-03-27 20:00:19 +0100

    Bundle environment configs

---
+++ config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	ProxyCurlApiKey = "PROXYCURL_APIKEY"
+	Port            = "PORT"
+)
+
+type Environment map[string]string
+
+func New() (Environment, error) {
+	env := make(Environment)
+	for _, key := range []string{
+		Port,
+		ProxyCurlApiKey,
+	} {
+		v, ok := os.LookupEnv(key)
+		if !ok {
+			return nil, fmt.Errorf("can't find %q in environment", key)
+		}
+		env[key] = v
+	}
+	return env, nil
+}
