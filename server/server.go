02549acb44b1cb110d794880eda2d1af5d46fe8b
Faithfulness Alamu <vaguemail369@gmail.com>
2022-03-27 21:38:21 +0100

    Get access token and email from code

--- server/server.go
+++ server/server.go
@@ -1,14 +1,19 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/handlers"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/thealamu/linkedinsignin/config"
 	"github.com/thealamu/linkedinsignin/linkedin"
 	"html/template"
+	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -22,7 +27,7 @@ func Start(logger zerolog.Logger, env config.Environment, linkedinService linked
 	logOutput := os.Stdout
 
 	e.Handle("/", handlers.LoggingHandler(logOutput, rootHandler(logger, env)))
-	e.Handle("/auth/linkedin/callback", handlers.LoggingHandler(logOutput, linkedInCallbackHandler(logger)))
+	e.Handle("/auth/linkedin/callback", handlers.LoggingHandler(logOutput, linkedInCallbackHandler(logger, env)))
 
 	srv := &http.Server{
 		ReadTimeout:  10 * time.Second,
@@ -62,7 +67,7 @@ func rootHandler(logger zerolog.Logger, env config.Environment) http.HandlerFunc
 	}
 }
 
-func linkedInCallbackHandler(logger zerolog.Logger) http.HandlerFunc {
+func linkedInCallbackHandler(logger zerolog.Logger, env config.Environment) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		code := r.URL.Query().Get("code")
 		if code == "" {
@@ -71,5 +76,89 @@ func linkedInCallbackHandler(logger zerolog.Logger) http.HandlerFunc {
 			return
 		}
 		logger.Info().Str("Code", code).Msg("Successfully received code in redirect url")
+
+		// get access token
+		endpoint := "https://www.linkedin.com/oauth/v2/accessToken"
+		redirectURL := fmt.Sprintf("%s://%s/auth/linkedin/callback", "http", r.Host)
+
+		data := url.Values{}
+		data.Set("grant_type", "authorization_code")
+		data.Set("code", code)
+		data.Set("client_id", env[config.ClientID])
+		data.Set("client_secret", env[config.ClientSecret])
+		data.Set("redirect_uri", redirectURL)
+
+		req, err := http.NewRequest(http.MethodPost, endpoint, strings.NewReader(data.Encode()))
+		if err != nil {
+			logger.Err(err).Msg("Failed to create HTTP request")
+			http.Error(w, "Failed to get access token", http.StatusInternalServerError)
+			return
+		}
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			logger.Err(err).Msg("Failed to do request")
+			http.Error(w, "Failed to get access token", http.StatusInternalServerError)
+			return
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			log.Err(fmt.Errorf("expected status code 200, got %d", resp.StatusCode)).Msg("Request failed")
+			http.Error(w, "Failed to get access token", http.StatusInternalServerError)
+			return
+		}
+		defer resp.Body.Close()
+
+		var payload AccessTokenResponse
+
+		rawJSON, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			logger.Err(err).Msg("Failed to read response body")
+			http.Error(w, "Failed to get access token", http.StatusInternalServerError)
+			return
+		}
+		err = json.Unmarshal(rawJSON, &payload)
+		if err != nil {
+			logger.Err(err).Msg("Failed to unmarshal response body")
+			http.Error(w, "Failed to get access token", http.StatusInternalServerError)
+			return
+		}
+
+		logger.Info().Str("Token", payload.AccessToken).Str("Expires", payload.ExpiresIn).Msg("Successfully received access token")
+
+		// Get user email
+		email, err := getUserEmail(payload.AccessToken)
+		if err != nil {
+			logger.Err(err).Msg("Failed to get user email")
+			http.Error(w, "Failed to get user email", http.StatusInternalServerError)
+			return
+		}
+
+		logger.Info().Str("Email", email).Msg("Successfully received user email")
 	}
+}
+
+func getUserEmail(token string) (string, error) {
+	endpoint := "https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))"
+
+	req, err := http.NewRequest(http.MethodGet, endpoint, nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	var payload EmailResponse
+	err = json.NewDecoder(resp.Body).Decode(&payload)
+	if err != nil {
+		return "", err
+	}
+
+	return payload.HandleContent.EmailAddress, nil
 }
--- server/types.go
+++ server/types.go
@@ -4,4 +4,16 @@ type (
 	IndexTemplateData struct {
 		AuthAPI string
 	}
+
+	AccessTokenResponse struct {
+		AccessToken string `json:"access_token"`
+		ExpiresIn   string `json:"expires_in"`
+	}
+
+	EmailResponse struct {
+		Handle        string `json:"handle"`
+		HandleContent struct {
+			EmailAddress string `json:"emailAddress"`
+		} `json:"handle~"`
+	}
 )
