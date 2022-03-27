ab9f68cbf7d2bd8fd29cf771f5bdafeb06e3744c
Faithfulness Alamu <vaguemail369@gmail.com>
2022-03-27 19:09:54 +0100

    Scaffold linkedin service

---
+++ linkedin/linkedin.go
@@ -0,0 +1,40 @@
+package linkedin
+
+import (
+	"fmt"
+	"github.com/rs/zerolog"
+)
+
+type (
+	Service interface {
+		GetProfile(GetProfileInput) (GetProfileOutput, error)
+	}
+
+	GetProfileInput struct {
+	}
+
+	GetProfileOutput struct {
+		FullName       string
+		ProfilePhoto   string
+		Location       string
+		Email          string
+		Phone          string
+		WorkExperience interface{}
+	}
+
+	lkd struct {
+		logger zerolog.Logger
+		apiKey string
+	}
+)
+
+func New(logger zerolog.Logger, apiKey string) Service {
+	return &lkd{
+		logger: logger,
+		apiKey: apiKey,
+	}
+}
+
+func (l *lkd) GetProfile(input GetProfileInput) (GetProfileOutput, error) {
+	return GetProfileOutput{}, fmt.Errorf("not implemented")
+}
