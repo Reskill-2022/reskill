df732f005a0d944f4dd1e14f9d573ed9da5c9be5
Faithfulness Alamu <vaguemail369@gmail.com>
2022-04-07 01:53:24 +0100

    Scaffold user update

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -43,7 +43,67 @@ func (u *UserController) CreateUser(userCreator repository.UserCreator) echo.Han
 
 func (u *UserController) UpdateUser(userUpdater repository.UserUpdater) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return fmt.Errorf("not implemented")
+		ctx := c.Request().Context()
+
+		var requestBody requests.UpdateUserRequest
+
+		err := c.Bind(&requestBody)
+		if err != nil {
+			return HandleError(c, err, http.StatusBadRequest)
+		}
+
+		update := model.User{
+			Email: c.Param("email"),
+		}
+
+		{
+			if requestBody.Representation != "" {
+				update.Representation = requestBody.Representation
+			}
+
+			if requestBody.Gender != "" {
+				update.Gender = requestBody.Gender
+			}
+
+			if requestBody.AgeGroup != "" {
+				update.AgeGroup = requestBody.AgeGroup
+			}
+
+			if requestBody.EmploymentStatus != "" {
+				update.EmploymentStatus = requestBody.EmploymentStatus
+			}
+
+			if requestBody.HighestSchool != "" {
+				update.HighestSchool = requestBody.HighestSchool
+			}
+
+			if requestBody.CanWorkInUSA != "" {
+				update.CanWorkInUSA = requestBody.CanWorkInUSA
+			}
+
+			if requestBody.LearningTrack != "" {
+				update.LearningTrack = requestBody.LearningTrack
+			}
+
+			if requestBody.TechExperience != "" {
+				update.TechExperience = requestBody.TechExperience
+			}
+
+			if requestBody.HoursPerWeek != "" {
+				update.HoursPerWeek = requestBody.HoursPerWeek
+			}
+
+			if requestBody.Referral != "" {
+				update.Referral = requestBody.Referral
+			}
+		}
+
+		user, err := userUpdater.UpdateUser(ctx, update)
+		if err != nil {
+			return HandleError(c, err, http.StatusInternalServerError)
+		}
+
+		return HandleSuccess(c, user, http.StatusOK)
 	}
 }
 
--- requests/requests.go
+++ requests/requests.go
@@ -4,4 +4,17 @@ type (
 	CreateUserRequest struct {
 		Email string `json:"email"`
 	}
+
+	UpdateUserRequest struct {
+		Representation   string `json:"representation"`
+		Gender           string `json:"gender"`
+		AgeGroup         string `json:"age_group"`
+		EmploymentStatus string `json:"employment_status"`
+		HighestSchool    string `json:"highest_school"`
+		CanWorkInUSA     string `json:"can_work_in_usa"`
+		LearningTrack    string `json:"learning_track"`
+		TechExperience   string `json:"tech_experience"`
+		HoursPerWeek     string `json:"hours_per_week"`
+		Referral         string `json:"referral"`
+	}
 )
