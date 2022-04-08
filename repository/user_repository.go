0eb0616c68d79bb278f3d60b3180e58090f71b03
Faithfulness Alamu <vaguemail369@gmail.com>
2022-04-08 11:54:03 +0100

    In-mem implementation of repo

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -6,22 +6,37 @@ import (
 	"github.com/thealamu/linkedinsignin/model"
 )
 
-type UserRepository struct{}
+type UserRepository struct {
+	data map[string]*model.User
+}
 
 var _ UserRepositoryInterface = (*UserRepository)(nil)
 
 func NewUserRepository() *UserRepository {
-	return &UserRepository{}
+	return &UserRepository{
+		data: make(map[string]*model.User),
+	}
 }
 
 func (u *UserRepository) CreateUser(ctx context.Context, user model.User) (*model.User, error) {
-	return nil, fmt.Errorf("not implemented")
+	if _, ok := u.data[user.Email]; ok {
+		return nil, fmt.Errorf("user with email %s already exists", user.Email)
+	}
+	u.data[user.Email] = &user
+	return &user, nil
 }
 
 func (u *UserRepository) UpdateUser(ctx context.Context, user model.User) (*model.User, error) {
-	return nil, fmt.Errorf("not implemented")
+	if _, ok := u.data[user.Email]; !ok {
+		return nil, fmt.Errorf("user with email %s does not exist", user.Email)
+	}
+	u.data[user.Email] = &user
+	return &user, nil
 }
 
 func (u *UserRepository) GetUser(ctx context.Context, email string) (*model.User, error) {
-	return nil, fmt.Errorf("not implemented")
+	if _, ok := u.data[email]; !ok {
+		return nil, fmt.Errorf("user with email %s does not exist", email)
+	}
+	return u.data[email], nil
 }
