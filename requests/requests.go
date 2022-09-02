7eceb8439feeecc619a21f9844dc33937fcc80b8
Faithfulness Alamu <vaguemail369@gmail.com>
2022-09-02 01:54:29 +0100

    Add form checks

--- requests/requests.go
+++ requests/requests.go
@@ -15,7 +15,7 @@ type (
 		AgeGroup               string `json:"age_group"`
 		EmploymentStatus       string `json:"employment_status"`
 		HighestSchool          string `json:"highest_school"`
-		OptionalMajor          string `json:"field_of_study"`
+		OptionalMajor          string `json:"field__of__study"`
 		CanWorkInUSA           string `json:"can_work_in_usa"`
 		LearningTrack          string `json:"learning_track"`
 		TechExperience         string `json:"tech_experience"`
